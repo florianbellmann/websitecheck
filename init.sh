4bdb5f38d2e38f62673916224f62c02c170383bd
flo <flo>
2020-12-25 17:47:21 +0100

    modified init script

--- init.sh
+++ init.sh
@@ -2,8 +2,7 @@
 
 git submodule init
 git submodule update
-cd imgdiff
-sed -i 's/platforms(.*)/platforms("darwin/amd64")/' scripts/build.sh
-./scripts/build.sh
 
-cd ..
+sed -i 's/platforms\(.*\)/platforms\(\"darwin\/amd64\"\)/g' imgdiff/scripts/build.sh
+
+./scripts/build.sh
