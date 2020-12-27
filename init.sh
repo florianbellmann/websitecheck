2789d95213231c36d6e7eaa5c90184e93ee2515b
flo <flo>
2020-12-27 12:16:39 +0100

    removed debug cat command

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 git submodule init
 git submodule update
 
-sed 's/platforms\=\(.*\)/platforms\=\(\"linux\/386\"\)/' -i imgdiff/scripts/build.sh && cat imgdiff/scripts/build.sh
+sed 's/platforms\=\(.*\)/platforms\=\(\"linux\/386\"\)/' -i imgdiff/scripts/build.sh 
 
 ./imgdiff/scripts/build.sh
